d00f6de4ae3d24269ad4d1e39830117ac6f3f348
alelanda135 <alejandra.landaeta@ucb.edu.bo>
2024-09-20 13:11:17 -0400

    redireccion opciones del navbar

--- public/app.js
+++ public/app.js
@@ -12,6 +12,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Definir las rutas para redirección
+app.get('/buscador', (req, res) => {
+    res.send('<h1>Página del Buscador de Archivos</h1><p>Aquí puedes buscar archivos.</p>');
+});
+
+app.get('/agregar', (req, res) => {
+    res.send('<h1>Página para Agregar Archivo</h1><p>Aquí puedes agregar un nuevo archivo.</p>');
+});
+
+app.get('/reportes', (req, res) => {
+    res.send('<h1>Página de Reportes y Estadísticas</h1><p>Aquí puedes ver los reportes.</p>');
+});
+
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);

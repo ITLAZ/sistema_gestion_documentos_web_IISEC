4f38c830eb27c65ea032c6a14f44f780499bbcd6
bryan2251 <bryan.castro@ucb.edu.bo>
2025-01-22 18:47:20 -0400

    env puerto

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
   //Documentacion
   const config = new DocumentBuilder()
   .setTitle('Mi API')
@@ -18,6 +20,10 @@ async function bootstrap() {
   // Puerto abierto
   app.enableCors();
 
-  await app.listen(3000);
+// Usa el puerto desde el archivo .env, o por defecto 3000 si no está definido
+  const port = configService.get<number>('PORT') || 3000;
+  await app.listen(port);
+
+  console.log(`Aplicación corriendo en: http://localhost:${port}`);
 }
 bootstrap();
